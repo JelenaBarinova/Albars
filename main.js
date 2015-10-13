b000d98b6d5f0216ac5043d7255d3db699b19e2a
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-10-13 11:41:03 -0700

    vartop fixed

--- main.js
+++ main.js
@@ -30471,6 +30471,7 @@ let TopMenuComponent = React.createClass ({displayName: "TopMenuComponent",
   switchLanguage: function(key, event) {
 	  //event.preventDefault();
     MenuActions.switchLanguage(key);
+    console.log('add ?lang=key to the path');
     console.log('stitchLangugae called');
     console.log(key);
 	},
@@ -30478,6 +30479,10 @@ let TopMenuComponent = React.createClass ({displayName: "TopMenuComponent",
   render() {
 
     return (
+    React.createElement("div", {id: "ru"}, 
+      React.createElement("div", {className: "templatemo-top-bar", id: "templatemo-top"}
+              
+      ), 
       React.createElement("div", {className: "templatemo-top-menu"}, 
         React.createElement("div", {className: "container"}, 
           React.createElement("div", {className: "navbar navbar-default", role: "navigation"}, 
@@ -30490,28 +30495,29 @@ let TopMenuComponent = React.createClass ({displayName: "TopMenuComponent",
                   React.createElement("span", {className: "icon-bar"})
                 ), 
                 
-                React.createElement("a", {href: "#", className: "navbar-brand"}, React.createElement("img", {src: "images/albars_logo.png", alt: "Urbanic template", title: "Urbanic Template"}))
+                React.createElement("a", {href: "#", className: "navbar-brand"}, React.createElement("img", {src: "images/albars_logo.png", alt: "Albars", title: "Albars"}))
               ), 
               React.createElement("div", {className: "navbar-collapse collapse", id: "templatemo-nav-bar"}, 
-                React.createElement("div", null, 
-                  React.createElement("ul", {className: "nav navbar-nav navbar-right", style: {marginTop: '40px'}}, 
-                    React.createElement("li", {className: "active"}, React.createElement("a", {href: "#templatemo-top"}, this.state.menuData.home.toUpperCase())), 
-                    React.createElement("li", null, React.createElement("a", {href: "#templatemo-about"}, this.state.menuData.team.toUpperCase())), 
-                    React.createElement("li", null, React.createElement("a", {href: "#templatemo-portfolio"}, this.state.menuData.services.toUpperCase())), 
-                    React.createElement("li", null, React.createElement("a", {href: "#templatemo-blog"}, this.state.menuData.blog.toUpperCase())), 
-                    React.createElement("li", null, React.createElement("a", {href: "#templatemo-blog"}, this.state.menuData.clients.toUpperCase())), 
-                    React.createElement("li", null, React.createElement("a", {href: "#templatemo-contact"}, this.state.menuData.contact.toUpperCase())), 
+               
+                React.createElement("ul", {className: "nav navbar-nav navbar-right", style: {marginTop: '40px'}}, 
+                  React.createElement("li", {className: "active"}, React.createElement("a", {href: "#templatemo-top"}, this.state.menuData.home.toUpperCase())), 
+                  React.createElement("li", null, React.createElement("a", {href: "#templatemo-about"}, this.state.menuData.team.toUpperCase())), 
+                  React.createElement("li", null, React.createElement("a", {href: "#templatemo-portfolio"}, this.state.menuData.services.toUpperCase())), 
+                  React.createElement("li", null, React.createElement("a", {href: "#templatemo-blog"}, this.state.menuData.blog.toUpperCase())), 
+                  React.createElement("li", null, React.createElement("a", {href: "#templatemo-blog"}, this.state.menuData.clients.toUpperCase())), 
+                  React.createElement("li", null, React.createElement("a", {href: "#templatemo-contact"}, this.state.menuData.contact.toUpperCase())), 
 
-                    React.createElement("li", null, React.createElement("a", {href: "#/en", onClick: this.switchLanguage.bind(this,'en_US')}, React.createElement("img", {src: "images/flags/us.png", alt: "en_US", title: "English"}))), 
-                    React.createElement("li", null, React.createElement("a", {href: "#/ru", onClick: this.switchLanguage.bind(this,'ru_RU')}, React.createElement("img", {src: "images/flags/ru.png", alt: "ru_RU", title: "Русский"})))
-                                                             
-                  )
+                  React.createElement("li", null, React.createElement("a", {href: "#", onClick: this.switchLanguage.bind(this,'en_US')}, React.createElement("img", {src: "images/flags/us.png", alt: "en_US", title: "English"}))), 
+                  React.createElement("li", null, React.createElement("a", {href: "#ru", onClick: this.switchLanguage.bind(this,'ru_RU')}, React.createElement("img", {src: "images/flags/ru.png", alt: "ru_RU", title: "Русский"})))
+                                                          
                 )
+                               
               )
             )
           )
         )
-      )  
+      )
+      ) 
     );
   }
 });
