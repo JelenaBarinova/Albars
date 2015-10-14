00378ce0621d2de68482192acc7a9c5cbd594f16
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-10-13 22:05:19 -0700

    46

--- main.js
+++ main.js
@@ -20692,7 +20692,7 @@ let InitializeActions = {
 
 module.exports = InitializeActions;
 
-},{"../api/api.js":166,"../constants/action-types.js":170,"../dispatcher/dispatcher.js":171}],165:[function(require,module,exports){
+},{"../api/api.js":166,"../constants/action-types.js":169,"../dispatcher/dispatcher.js":170}],165:[function(require,module,exports){
 "use strict";
 
 let Dispatcher = require('../dispatcher/dispatcher.js');
@@ -20711,7 +20711,7 @@ let MenuActions = {
 
 module.exports = MenuActions;
 
-},{"../constants/action-types.js":170,"../dispatcher/dispatcher.js":171}],166:[function(require,module,exports){
+},{"../constants/action-types.js":169,"../dispatcher/dispatcher.js":170}],166:[function(require,module,exports){
 "use strict";
 
 let Api = {
@@ -20735,7 +20735,7 @@ module.exports = Api;
 let React = require('react');
 
 let TopMenuComponent = require('./top-menu-component.jsx');
-let CarouselComponent = require('./carousel-component.jsx');
+//let CarouselComponent = require('./carousel-component.jsx');
 /*let ServicesComponent = require('./services-component.jsx');
 let TeamComponent = require('./team-component.jsx');
 let BlogComponent = require('./blog-component.jsx');
@@ -20750,8 +20750,7 @@ let App = React.createClass ({displayName: "App",
   render: function() {
     return (
       React.createElement("div", null, 
-	  	  React.createElement(TopMenuComponent, null), 
-        React.createElement(CarouselComponent, null)
+	  	  React.createElement(TopMenuComponent, null)
         
 	    )
     );
@@ -20760,101 +20759,7 @@ let App = React.createClass ({displayName: "App",
 
 module.exports = App;
 
-},{"./carousel-component.jsx":168,"./top-menu-component.jsx":169,"react":163}],168:[function(require,module,exports){
-"use strict";
-let React = require('react');
-let AppStore = require('../stores/app-store.js');
-    
-let CarouselComponent = React.createClass ({displayName: "CarouselComponent",
-  
-  getInitialState: function() {
-    return {    
-       carouselData: AppStore.getCarousel()
-    };
-  },
-  componentWillMount: function() {
-    AppStore.addChangeListener(this._onChange); 
-  },
- 
-  
-  componentWillUnmount: function() {
-    AppStore.removeChangeListener(this._onChange); 
-  },
-  
-  _onChange: function() {
-    console.log('changing courasel component');
-    this.setState({ 
-      carouselData: AppStore.getCarousel() 
-    });
-  },
-  render: function() {
-  
-    return (
-      React.createElement("div", null, 
-        React.createElement("div", {id: "templatemo-carousel", className: "carousel slide", "data-ride": "carousel"}, 
-          React.createElement("ol", {className: "carousel-indicators"}, 
-            React.createElement("li", {"data-target": "#templatemo-carousel", "data-slide-to": "0", className: "active"}), 
-            React.createElement("li", {"data-target": "#templatemo-carousel", "data-slide-to": "1"}), 
-            React.createElement("li", {"data-target": "#templatemo-carousel", "data-slide-to": "2"})
-          ), 
-          React.createElement("div", {className: "carousel-inner"}, 
-            React.createElement("div", {className: "item active"}, 
-              React.createElement("div", {className: "container"}, 
-                React.createElement("div", {className: "carousel-caption"}, 
-                  React.createElement("h1", null, this.state.carouselData.list[0].title), 
-                  React.createElement("p", null, this.state.carouselData.list[0].content), 
-                  React.createElement("p", null, 
-                  (() => {
-                      if (this.state.carouselData.list[0].url) {
-                        return React.createElement("a", {className: "btn btn-lg btn-blue", href: this.state.carouselData.list[0].url, role: "button"}, this.state.carouselData.list[0].more);
-                      }
-                    })()
-                  )
-                )
-              )
-            ), 
-            React.createElement("div", {className: "item"}, 
-              React.createElement("div", {className: "container"}, 
-                React.createElement("div", {className: "carousel-caption"}, 
-                  React.createElement("h1", null, this.state.carouselData.list[1].title), 
-                  React.createElement("p", null, this.state.carouselData.list[1].content), 
-                  React.createElement("p", null, 
-                    (() => {
-                      if (this.state.carouselData.list[1].url) {
-                        return React.createElement("a", {className: "btn btn-lg btn-blue", href: this.state.carouselData.list[1].url, role: "button"}, this.state.carouselData.list[1].more);
-                      }
-                    })()
-                  )
-                )
-              )
-            ), 
-            React.createElement("div", {className: "item active"}, 
-              React.createElement("div", {className: "container"}, 
-                React.createElement("div", {className: "carousel-caption"}, 
-                  React.createElement("h1", null, this.state.carouselData.list[2].title), 
-                  React.createElement("p", null, this.state.carouselData.list[2].content), 
-                  React.createElement("p", null, 
-                    (() => {
-                      if (this.state.carouselData.list[2].url) {
-                        return React.createElement("a", {className: "btn btn-lg btn-blue", href: this.state.carouselData.list[2].url, role: "button"}, this.state.carouselData.list[2].more);
-                      }
-                    })()
-                  )
-                )
-              )
-            )
-          ), 
-          React.createElement("a", {className: "left carousel-control", href: "#templatemo-carousel", "data-slide": "prev"}, React.createElement("span", {className: "glyphicon glyphicon-chevron-left"})), 
-          React.createElement("a", {className: "right carousel-control", href: "#templatemo-carousel", "data-slide": "next"}, React.createElement("span", {className: "glyphicon glyphicon-chevron-right"}))
-        )
-      )
-    );
-  }
-});
-
-module.exports = CarouselComponent;
-
-},{"../stores/app-store.js":173,"react":163}],169:[function(require,module,exports){
+},{"./top-menu-component.jsx":168,"react":163}],168:[function(require,module,exports){
 "use strict";
 let React = require('react');
 let MenuActions = require('../actions/menu-actions.js');
@@ -20938,7 +20843,7 @@ let TopMenuComponent = React.createClass ({displayName: "TopMenuComponent",
 
 module.exports = TopMenuComponent;
 
-},{"../actions/menu-actions.js":165,"../stores/app-store.js":173,"react":163}],170:[function(require,module,exports){
+},{"../actions/menu-actions.js":165,"../stores/app-store.js":172,"react":163}],169:[function(require,module,exports){
 "use strict";
 
 let keyMirror = require('react/lib/keyMirror');
@@ -20948,14 +20853,14 @@ module.exports = keyMirror({
 	SWITCH_LANGUAGE: null
 });
 
-},{"react/lib/keyMirror":148}],171:[function(require,module,exports){
+},{"react/lib/keyMirror":148}],170:[function(require,module,exports){
 "use strict";
 
 let Dispatcher = require('flux').Dispatcher;
 
 module.exports = new Dispatcher();
 
-},{"flux":5}],172:[function(require,module,exports){
+},{"flux":5}],171:[function(require,module,exports){
 "use strict";
 
 let React   = require('react');
@@ -20967,7 +20872,7 @@ InitializeActions.initApp();
 
 React.render(React.createElement(App, null), document.getElementById('HomePage'));
 
-},{"./actions/initialize-actions.js":164,"./components/app.jsx":167,"react":163}],173:[function(require,module,exports){
+},{"./actions/initialize-actions.js":164,"./components/app.jsx":167,"react":163}],172:[function(require,module,exports){
 "use strict";
 
 let Dispatcher = require('../dispatcher/dispatcher.js');
@@ -21033,4 +20938,4 @@ Dispatcher.register(function(action){
 
 module.exports = AppStore;
 
-},{"../api/api.js":166,"../constants/action-types.js":170,"../dispatcher/dispatcher.js":171,"events":3,"object-assign":8}]},{},[172]);
+},{"../api/api.js":166,"../constants/action-types.js":169,"../dispatcher/dispatcher.js":170,"events":3,"object-assign":8}]},{},[171]);
